813553f38b4dd8a2f01355d72322d30d54156a05
Shuryansh07 <shuryanshgupta8050@gmail.com>
2025-07-02 18:51:36 +0530

    Day 1: Moved tasks to data file, mapped over tasks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import TaskItem from "./components/TaskItem";
+import tasks from "./data/tasks";
 
 function App() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>DevTasks - Developer Task Manager</h1>
-      <TaskItem title="Fix the navbar bug" type="Bug" status="Incomplete" />
-      <TaskItem title="Add login feature" type="Feature" status="complete" />
-      <TaskItem
-        title="Watch React Tutorial"
-        type="Learning"
-        status="Incomplete"
-      />
+
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          title={task.title}
+          type={task.type}
+          status={task.status}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/data/tasks.js
@@ -0,0 +1,12 @@
+const tasks = [
+  { id: 1, title: "Fix navbar bug", type: "Bug", status: "Incomplete" },
+  { id: 2, title: "Add login feature", type: "Feature", status: "Incomplete" },
+  {
+    id: 3,
+    title: "Watch React Tutorial",
+    type: "Learning",
+    stats: "Incomplete",
+  },
+];
+
+export default tasks;
